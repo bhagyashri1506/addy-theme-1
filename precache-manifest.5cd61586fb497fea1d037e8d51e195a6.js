ff46a8fefca427435372cb34d52c5ddd9ad8d55e
Harshit <harshitupadhyay872@gmail.com>
2023-05-16 19:35:45 +0530

    Updates

---
+++ precache-manifest.5cd61586fb497fea1d037e8d51e195a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e327a705ea4da2420f3",
+    "url": "/addy-theme-1/static/css/main.d7d60173.chunk.css"
+  },
+  {
+    "revision": "4e327a705ea4da2420f3",
+    "url": "/addy-theme-1/static/js/main.4e327a70.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/addy-theme-1/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "c48bcef1b005a156a1c7",
+    "url": "/addy-theme-1/static/css/2.64ff41d3.chunk.css"
+  },
+  {
+    "revision": "c48bcef1b005a156a1c7",
+    "url": "/addy-theme-1/static/js/2.c48bcef1.chunk.js"
+  },
+  {
+    "revision": "dacee4cb8ad02e775c6c",
+    "url": "/addy-theme-1/static/js/runtime~main.dacee4cb.js"
+  },
+  {
+    "revision": "a4c88ad765b83a4cc3fa2c3b59aa1867",
+    "url": "/addy-theme-1/static/media/marquee.a4c88ad7.jpg"
+  },
+  {
+    "revision": "8ef4dbf6b59003f8f156c0d589666f6d",
+    "url": "/addy-theme-1/static/media/logo.8ef4dbf6.png"
+  },
+  {
+    "revision": "e85838a4710f57a7dbb3a3c158b95c4c",
+    "url": "/addy-theme-1/static/media/flag.e85838a4.png"
+  },
+  {
+    "revision": "ae0bfa82f983cf1832c7038bac0fb9c5",
+    "url": "/addy-theme-1/static/media/auto-ewallet.ae0bfa82.jpg"
+  },
+  {
+    "revision": "a2465140fa88edf31673a01e47788710",
+    "url": "/addy-theme-1/static/media/online.a2465140.jpg"
+  },
+  {
+    "revision": "4b0662c4c4bd847e9aca903917f387fa",
+    "url": "/addy-theme-1/static/media/ATM.4b0662c4.jpg"
+  },
+  {
+    "revision": "4a61bfce39c1f7636f4eb2fdbc6789ea",
+    "url": "/addy-theme-1/index.html"
+  }
+];
